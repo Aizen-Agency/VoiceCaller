d1b5b67da23ae119e05f5b7e46dbda397fffa1bc
hritikp <hritik.pihal@oneture.com>
2024-10-29 23:44:03 +0530

    Added mark and clear tags

--- main.py
+++ main.py
@@ -109,6 +109,7 @@ async def proxy(client_ws, path):
     conn_start = time.time()
 
     streamSid = ""
+    prompt_count = 0
     
     async with deepgram_connect() as deepgram_ws:
         async def deepgram_sender(deepgram_ws):
@@ -126,8 +127,8 @@ async def proxy(client_ws, path):
                         # Get response from OpenAI API
                         response = await get_openai_response(transcript, streamSid)
                         payload =  text_to_speech_base64(response)
+                        prompt_count = prompt_count + 1
                         try:
-                            
                            await client_ws.send(json.dumps({
                             "event": "media",
                             "streamSid": streamSid,
@@ -159,6 +160,7 @@ async def proxy(client_ws, path):
         async def client_receiver(client_ws):
             nonlocal streamSid 
             nonlocal audio_cursor
+            nonlocal prompt_count
             BUFFER_SIZE = 20 * 160
             buffer = bytearray(b'')
             empty_byte_received = False
@@ -190,7 +192,9 @@ async def proxy(client_ws, path):
                         streamSid = data['streamSid']
                         del conversation_history_map[streamSid]
                         break
-                    
+                    if data["event"] == "mark": 
+                        prompt_count = prompt_count - 1
+                        print(prompt_count)
 
                     if len(buffer) >= BUFFER_SIZE or empty_byte_received:
                         outbox.put_nowait(buffer)
