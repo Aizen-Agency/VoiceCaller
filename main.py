045a12088e02b771814a9356bd8c961e7c749b5e
hritikp <hritik.pihal@oneture.com>
2024-11-11 15:55:12 +0530

    deploy

--- main.py
+++ main.py
@@ -403,7 +403,7 @@ async def proxy(client_ws, path):
 #     await asyncio.Future()  # Keep the server running indefinitely
 
 async def main():
-    port = int(os.environ.get("PORT", 8000))  # Default to 5000 if PORT is not set
+    port = 8000  # Default to 5000 if PORT is not set
     print(f"port here {port}")
     proxy_server = await websockets.serve(proxy, '0.0.0.0', port)  # Bind to all interfaces
     print(f"Server started on port {port}")
